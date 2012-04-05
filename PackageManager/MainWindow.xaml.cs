c0610d93d2d459ab86ff6c0419f0f9178b82c192
Eric Schultz <wwahammy@gmail.com>
2012-04-05 12:54:48 -0500

    Inital commit of Big Gui

---
+++ PackageManager/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CoApp.GUI.PackageManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
